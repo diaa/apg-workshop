1ecc78965e7d5b21c9fbc0cedbfd33d49fa3a59e
pamir <pamir.erdem@gmail.com>
2022-03-17 14:15:25 +0000

    monitoring

---
+++ scripts/01-install-cli.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -L https://aka.ms/InstallAzureCli | bash
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
